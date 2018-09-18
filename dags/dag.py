b012660c6fd13ab0be312382a6e8d3f10ea697d6
Arjan Groen <a.groen1@BvM.local>
2018-09-18 15:15:20 +0200

    vaagheid2

--- dags/dag.py
+++ dags/dag.py
@@ -6,11 +6,12 @@ from godatadriven.operators.postgres_to_gcs import PostgresToGoogleCloudStorageO
 
 
 dag = DAG(
-    dag_id="my_first_dag",
+    dag_id="my_second_dag",
     schedule_interval="30 7 * * *",
     default_args={
         "owner": "airflow",
-        "start_date": dt.datetime(2018, 6, 20),
+        "start_date": dt.datetime(2018, 1, 1),
+        "end_date": dt.datetime(2018, 2, 1),
         "depends_on_past": True,
         "email_on_failure": True,
         "email": "airflow_errors@myorganisation.com",
@@ -21,13 +22,6 @@ dag = DAG(
 def print_exec_date(**context):
     print(context["execution_date"])
 
-
-my_task = PythonOperator(
-    task_id="task_name",
-    python_callable=print_exec_date,
-    provide_context=True,
-    dag=dag
-)
 
 pgsl_to_gcs = PostgresToGoogleCloudStorageOperator(
     task_id="pgsql_to_gcs",
