b66249703ebef2b5a949174e05ac81066cc97adf
Arjan Groen <a.groen1@BvM.local>
2018-09-19 10:04:45 +0200

    trial13

--- dags/dag.py
+++ dags/dag.py
@@ -6,7 +6,7 @@ from customops import HttpToGcsOperator
 
 
 dag = DAG(
-    dag_id="my_fourth_dag",
+    dag_id="my_fifth_dag",
     schedule_interval="30 7 * * *",
     default_args={
         "owner": "airflow",
